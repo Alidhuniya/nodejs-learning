5dbe6970e494d13503d5625931b53c79e2e4d4f9
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-09-22 14:10:23 +0545

    7-reading html file

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-import {multiply} from "./multiply.js"
+const http = require("http");
 
-const mult = multiply(2,3);
-console.log(mult);
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer ((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Hello World</h1> <br /> <h3>HTML </h3>");
+});
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
